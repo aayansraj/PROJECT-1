bc8727122044361a6661aa06941b72aba2a30ea0
aayansraj <aayansraj2007@gmail.com>
2026-03-18 12:45:17 +0530

    18-mar-26
    java script fundamentals

---
+++ index.js
@@ -0,0 +1,88 @@
+console.log("hello world")
+
+// let city = "Bangalore";
+// city = "Delhi";
+// console.log("city",city);
+
+// const country = "india";
+// country = "USA";
+// console.log("country",country);
+
+// let marks = 90;
+// let student = "Aayans";
+// let isPassed = true;
+// let x;
+// let y = null;
+// console.log("marks,student,isPassed,x,y---->",marks,student,isPassed)
+
+// let student ="Rahul";
+// let x=5;
+// let z=typeof x;
+// typeof student;
+// console.log("z--",z);
+
+//NUMBER
+
+// let a= 10;
+// let b = 3.5;
+
+// let inf = Infinity;
+// let notANumber = NaN;
+
+// console.log("type of inf",typeof inf);
+// console.log("type of nan",typeof NaN);
+
+// console.log(typeof notANumber);
+
+// let age = 20;
+// Number(age); // 20
+// parseInt("10.5"); // 10
+// console.log("age-->", typeof Number (age));
+// console.log(typeof parseInt(10.5));
+
+
+//Strings
+
+// let firstName = "Aayans";  
+// let course = 'JavaScript';
+
+// firstName.length    
+// firstName.toUpperCase()
+// firstName.toLowerCase()
+// firstName.includes("Aay")
+
+// console.log(firstName.includes("java"));
+
+// let first = "Aayans";
+// let last = "Raj";
+
+// let full = first + " " + last;
+// console.log(full);
+
+
+// Template Literals
+
+// let firstName = "Aayans";
+// let age = 24;
+
+// let msg = `My name is ${firstName} and my age ${age}`;
+
+// console.log("message",msg);
+
+// let a = 10, b = 20;
+// let sum =`Sum is ${a + b}`;
+
+// console.log("sum",sum);
+
+// NAN
+
+let a = 10;
+let b = "abc"
+
+let result = a * b; //NAN
+console.log(typeof result); //"Number"
+
+console.log("5"+5);
+console.log("5"*5);
+console.log(25*"abc");
+console.log(5+5);
